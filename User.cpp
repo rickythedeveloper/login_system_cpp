d8985cd5591703e9a11a478122114fff3ae9de6e
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-08-24 22:02:51 +0100

    Check for valid chars instead of invalid chars

--- User.cpp
+++ User.cpp
@@ -8,20 +8,22 @@ User::User(string a, string b) {
 string User::getUsername() { return username; }
 string User::getPassword() { return password; }
 
-const int invalidUsernameCharCount = 1;
-const char invalidUsernameChars[invalidUsernameCharCount] = { ' ' };
+const string alphabetLowercase = "abcdefghijklmnopqrstuvwxyz";
+const string alphabetUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string numbers = "0123456789";
+
+const string validUsernameChars = alphabetLowercase + alphabetUppercase + numbers;
 bool User::isValidUsername(string username) {
-	for (int i = 0; i < invalidUsernameCharCount; i++) {
-		if (username.find(invalidUsernameChars[i]) != string::npos) return false;
+	for (int i = 0; i < username.size(); i++) {
+		if (validUsernameChars.find(username[i]) == string::npos) return false;
 	}
 	return true;
 }
 
-const int invalidPasswordCharCount = 1;
-const char invalidPasswordChars[invalidPasswordCharCount] = { ' ' };
+const string validPasswordChars = alphabetLowercase + alphabetUppercase + numbers;
 bool User::isValidPassword(string password) {
-	for (int i = 0; i < invalidPasswordCharCount; i++) {
-		if (password.find(invalidPasswordChars[i]) != string::npos) return false;
+	for (int i = 0; i < password.size(); i++) {
+		if (validPasswordChars.find(password[i]) == string::npos) return false;
 	}
 	return true;
 }
